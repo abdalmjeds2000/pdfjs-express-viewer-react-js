191e2d22c40c10b1e17e2cd9d673aae17902a1c3
AbdAlmjed Skaik <ab0d-33@hotmail.com>
2022-12-07 16:28:32 +0200

    first-commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useRef, useEffect, useState } from 'react';
+import WebViewer from '@pdftron/pdfjs-express-viewer';
+import './App.css';
+
+const App = () => {
+  const viewer = useRef(null);
+  
+  useEffect(() => {
+    WebViewer(
+      {
+        path: '/webviewer/lib',
+        initialDoc: '',
+        licenseKey: "7Yrd99cCi21Cu68XVLND"
+      },
+      viewer.current,
+    ).then((instance) => {
+        document.getElementById('localFilepicker').onchange = (e) => {
+          const file = e.target.files[0];
+          if (file) {
+            console.log(file)
+            instance.UI.loadDocument(file);
+          }
+        };
+        document.getElementById('inputPDFLink').onchange = (e) => {
+          e.preventDefault();
+            console.log(e)
+            instance.UI.loadDocument(document.getElementById('inputPDFLink').value);
+        };
+      });
+  }, []);
+
+
+
+  
+  return (
+    <div className="App">
+      <div className="header">SALIC PDF</div>
+      <div className='pdfviewer-container'>
+        <div className="filepicker-panel">
+          <div className='file-from-link'>
+            <label htmlFor='inputPDFLink'>Choose a file to view</label>
+            <input type="text" id='inputPDFLink' placeholder='Enter your PDF Link' />
+          </div>
+          <div className='file-from-local'>
+            <label htmlFor='localFilepicker'>Or choose your own</label>
+            <input className='custom-file-input' type="file" id='localFilepicker' accept="application/pdf" />
+          </div>
+        </div>
+        <div className="webviewer" ref={viewer}></div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
